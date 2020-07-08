4bbaa31558aa3d65496bf8c689a48936a27c0c9c
abrahaam39 <67434038+abrahaam39@users.noreply.github.com>
2020-07-08 22:09:48 +0200

    space

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
    public static void main(String[] args){
 
-      System.out.println("hello souna elghali ...weled bouh");
+      System.out.println("hello souna elghali ...weled bouh  ");
 }
 
 }
